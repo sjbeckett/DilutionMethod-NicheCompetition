0bff800ccd3e1984d2d07e1d6489dd8cad8d4d25
Stephen Beckett <beckett.stephenj@gmail.com>
2016-08-30 13:27:47 -0400

    Create workflow.R

---
+++ code/NicheCompetition/workflow.R
@@ -0,0 +1,19 @@
+# Part of the code used in:
+# Beckett and Weitz. Code for: A revised dilution methodology and implications for estimates of rates of plankton mortality.
+# 
+# MIT License
+
+
+
+#Perform dilution method experiments at low, intermediate and high levels of zooplankton pressure
+source("HML_Zooplankton.R")
+# runs simulated dilution experiments to create figures that show the performance of the classical dilution and Z-dilution methods at three indicated levels of zooplankton pressure
+source("GrowthRate_vs_ProportionWSW_Zooplankton.R")
+#Uses this data to plot the comparison of dilution curves between the two dilution methods at these zooplankton pressure levels
+
+
+
+
+#Perform dilution method experiments across a range of zooplankton pressure levels
+source("AllPressure_Zooplankton.R")
+# runs simulated dilution experiments to create figures that show the performance of the classical dilution and Z-dilution methods across a range of zooplankton pressure levels
