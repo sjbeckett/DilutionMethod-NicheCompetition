66b5d6e0952e882f34fbee0fd5cc21c7b971d8df
Stephen Beckett <beckett.stephenj@gmail.com>
2016-08-30 13:27:12 -0400

    Add files via upload

---
+++ code/main/LOADREQUIREMENTS.R
@@ -0,0 +1,11 @@
+# Part of the code used in:
+# Beckett and Weitz. Code for: A revised dilution methodology and implications for estimates of rates of plankton mortality.
+# 
+# MIT License
+
+#LOADREQUIREMENTS.R
+	#files required to run dilution method experiments
+	source("../main/models.R") # contains ecological models
+	source("../main/ANALYSIS.R") # contains function that performs and analyses simulations
+	library("deSolve") # contains functions for performing numerical solutions
+
